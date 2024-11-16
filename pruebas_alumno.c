457c9d60f49883f1a7c53ce38e7ce988def33be0
juan david chaparro <davidacosta312002@gmail.com>
2024-11-16 19:23:56 -0300

    primeras dos pruebas

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,28 @@
 #include "pa2m.h"
+#include "src/hash.h"
 
-void prueba_simple()
+void creo_un_hash_valido_pasandole_la_cantidad_correcta_me_debe_devolver_el_hash_creado_correctamente_con_la_cantidad_asignada()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i == j, "i es igual a j (%d == %d)", i, j);
-	pa2m_afirmar(2*i == 10, "2*i es igual a 2 (%d)", 2*i);
+	size_t cantidad = 20;
+	hash_t *hash = hash_crear(cantidad);
+	pa2m_afirmar(hash != NULL,"hash con mas de la cantidad correcta: se ha creado correctamente");
+	pa2m_afirmar(hash_cantidad(hash)==0,"hash vacio: no tiene elementos al inicializarse");
+	hash_destruir(hash);
 }
 
+void creo_un_hash_valido_pasandole_menos_de_la_cantidad_minima_me_debe_devolver_un_hash_con_la_cantidad_de_espacios_3()
+{
+	size_t cantidad = 1;
+	hash_t *hash = hash_crear(cantidad);
+	pa2m_afirmar(hash != NULL,"hash con menos de la cantidad minima: se ha creado correctamente");
+	pa2m_afirmar(hash_cantidad(hash)==0,"hash vacio: no tiene elementos al inicializarse");
+	hash_destruir(hash);
+}
 int main()
 {
-	pa2m_nuevo_grupo("============== ??? ===============");
-	prueba_simple();
+	pa2m_nuevo_grupo("Pruebas crear HASH");
+    creo_un_hash_valido_pasandole_la_cantidad_correcta_me_debe_devolver_el_hash_creado_correctamente_con_la_cantidad_asignada();
+    creo_un_hash_valido_pasandole_menos_de_la_cantidad_minima_me_debe_devolver_un_hash_con_la_cantidad_de_espacios_3();
 
 	return pa2m_mostrar_reporte();
 }
